f49f8f6818cf9c2ea17a5ddceaa304d2566580ff
Marius Žakevičius <marius.zakevicius@alchemetrics.co.uk>
2020-04-17 15:55:06 +0300

    Timer

---
+++ Timer/script.js
@@ -0,0 +1,24 @@
+const durationInput = document.getElementById("duration");
+const startButton = document.getElementById("start");
+const pauseButton = document.getElementById("pause");
+const circle = document.querySelector("circle");
+
+const perimeter = circle.getAttribute("r") * 2 * Math.PI;
+circle.setAttribute("stroke-dasharray", perimeter);
+
+let currentOffset = 0;
+let duration;
+const timer = new Timer(durationInput, startButton, pauseButton, {
+	onStart(totalDuration) {
+		duration = totalDuration;
+	},
+	onTick(timeRemaining) {
+		circle.setAttribute(
+			"stroke-dashoffset",
+			(perimeter * timeRemaining) / duration - perimeter
+		);
+	},
+	onComplete() {
+		console.log("completed");
+	},
+});
---
+++ Timer/timer.js
@@ -0,0 +1,45 @@
+class Timer {
+	constructor(durationInput, startButton, pauseButton, callbacks) {
+		this.durationInput = durationInput;
+		this.startButton = startButton;
+		this.pauseButton = pauseButton;
+		if (callbacks) {
+			this.onStart = callbacks.onStart;
+			this.onTick = callbacks.onTick;
+			this.onComplete = callbacks.onComplete;
+		}
+
+		this.startButton.addEventListener("click", this.start.bind(this));
+		this.pauseButton.addEventListener("click", this.pause.bind(this));
+	}
+
+	start() {
+		if (this.onStart) this.onStart(this.timeRemaining);
+		this.tick();
+		this.interval = setInterval(this.tick.bind(this), 50);
+	}
+
+	pause() {
+		clearInterval(this.interval);
+	}
+
+	onDurationChange() {}
+
+	tick() {
+		if (this.timeRemaining <= 0) {
+			this.pause();
+			if (this.onComplete) this.onComplete();
+		} else {
+			this.timeRemaining = this.timeRemaining - 0.05;
+			if (this.onTick) this.onTick(this.timeRemaining);
+		}
+	}
+
+	get timeRemaining() {
+		return parseFloat(this.durationInput.value);
+	}
+
+	set timeRemaining(time) {
+		this.durationInput.value = time.toFixed(2);
+	}
+}
